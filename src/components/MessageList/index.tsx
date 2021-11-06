4698dba077f084ab00800e0617d1cfab90688c01
coolluucaas <lucaspsanta@gmail.com>
2021-11-06 18:07:47 -0300

    + visualizacao autom das 3 msgs

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { api } from '../../services/api'
+import io from 'socket.io-client'
 
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
@@ -12,13 +13,31 @@ type Message = {
         avatar_url: string
     }
 }
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage)
+})
 
 export function MessageList() {
     const [messages, setMessages] = useState<Message[]>([])
 
     useEffect(() => {
+        const timer = setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => 
+                    [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+                )
+                messagesQueue.shift()
+            }
+        }, 3000)
+    }, [])
+
+    useEffect(() => {
         api.get('/messages/last3').then((response) => {
-            setMessages(response.data)           
+            setMessages(response.data)
         })
     }, [])
 
@@ -47,7 +66,7 @@ export function MessageList() {
                                 <span>{message.user.name}</span>
                             </div>
                         </li>
-                    );
+                    )
                 })}
             </ul>
         </div>
