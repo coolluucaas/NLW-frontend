37fdb09187866b650a8a4e067fc7510fbbdc7e85
coolluucaas <lucaspsanta@gmail.com>
2021-11-06 18:06:40 -0300

    + auth context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -9,8 +9,9 @@ type User = {
 }
 
 type AuthcontextData = {
-    user: User | null;
-    signInUrl: string;
+    user: User | null
+    signInUrl: string
+    signOut: () => void
 }
 
 export const AuthContext = createContext({} as AuthcontextData)
@@ -20,53 +21,57 @@ type AuthProvider = {
 }
 
 type AuthResponse = {
-    token: string;
+    token: string
     user: {
-        id: string;
-        avatar_url:string;
-        name: string;
-        login: string;
+        id: string
+        avatar_url: string
+        name: string
+        login: string
     }
 }
 
 export function AuthProvider(props: AuthProvider) {
-
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null)
 
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=31300017470e38ce3b60`
 
     async function signIn(githubCode: string) {
-        const response = await api.post<AuthResponse>('authenticate',{
-            code: githubCode
+        const response = await api.post<AuthResponse>('/authenticate', {
+            code: githubCode,
         })
 
-        const {token,user} = response.data;
+        const { token, user } = response.data
 
         localStorage.setItem('@dowhile:token', token)
 
+        api.defaults.headers.common.authorization = `Bearer ${token}`
+
         setUser(user)
+    }
 
-
+    async function signOut() {
+        setUser(null)
+        localStorage.removeItem('@dowhile:token')
     }
 
     useEffect(() => {
         const token = localStorage.getItem('@dowhile:token')
 
-        if(token) {
+        if (token) {
             api.defaults.headers.common.authorization = `Bearer ${token}`
 
-            api.get<User>('profile').then(response => {
+            api.get<User>('/profile').then((response) => {
                 setUser(response.data)
             })
         }
-    },[])
+    }, [])
 
     useEffect(() => {
         const url = window.location.href
         const hasGithubCode = url.includes('?code=')
 
         if (hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=')            
+            const [urlWithoutCode, githubCode] = url.split('?code=')
 
             window.history.pushState({}, '', urlWithoutCode)
 
@@ -74,8 +79,9 @@ export function AuthProvider(props: AuthProvider) {
         }
     }, [])
 
-
-    return <AuthContext.Provider value={{ signInUrl, user  }}>
-        {props.children}
-    </AuthContext.Provider>
+    return (
+        <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
 }
